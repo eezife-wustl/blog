459105a9b5f54a8ee7a6de6ecc56a56d13076af2
eezife-wustl <erikaezife@wustl.edu>
2022-05-25 19:30:21 -0500

    reverse order of posts

--- blog-me/pages/index.js
+++ blog-me/pages/index.js
@@ -15,7 +15,7 @@ import Link from 'next/link'
 export default function Home({posts}) {
   return (
     <div style={{ marginTop: "4rem"}}>
-            {posts.data 
+            {posts.data
              && posts.data.map((post) => (
                <div  key={post.id}>
                   <Link href={`/${post.attributes.Slug}`}>
@@ -34,6 +34,7 @@ export async function getStaticProps() {
         const response = await fetch("https://fast-shore-63405.herokuapp.com/api/posts");
         const posts = await response.json();
         console.log(posts);
+        posts.data.reverse();
         return {
             props: {posts},
         };
